f3227130375645f950930435cb0b72a35319aaf8
william vasilev <williamvasilev@gmail.com>
2022-09-20 15:59:01 +0100

    Add files via upload

---
+++ fundamentals.js
@@ -0,0 +1,44 @@
+const aCar = {
+    owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+      { name : "Pat Smith ", address : "- 1 Main Street" },
+      { name : "Sheila Dwyer", address : "- 2 High Street"}
+    ],
+    type : {
+      make : 'Toyota',
+      model : 'Corolla' ,
+      cc : '1.8'},
+      features : ['Parking assist', 'Alarm', 'Tow-bar'],
+    registration : {
+      year :'201',
+      countycode : 'WD',
+      number : '1058'
+    },
+    mileage : '10000',
+    colour : {
+      exterior : 'red',
+      interiorfabric : {
+        texture : 'leather',
+        shade : 'cream'
+      }
+
+    }
+  };
+  
+  console.log(aCar.owner + ' drives a ' + aCar.type.make)
+  console.log(aCar.registration.year + '-' + aCar.registration.countycode + '-' + aCar.registration.number)
+  console.log('It is a ' + aCar.colour.exterior + ' car,' + aCar.mileage + ' mileage, with ' + aCar.colour.interiorfabric.texture + ' interior')
+  console.log('First owner : ' + aCar.previous_owners[0].name + aCar.previous_owners[0].address )
+
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+} 
+
+ for (let i = 0 ; i < aCar.previous_owners.length ; i += 1){
+   console.log(aCar.previous_owners[i].name);
+ }
+
+ for (let p in aCar.type)  {
+  console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
